b94eabf09d846cefd40741d026e25b9e11ce0d60
Pouriya <pouriya.un@gmail.com>
2021-03-26 18:25:20 +0430

    first commit

---
+++ App_Blog/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+from . import views
+
+
+app_name = 'App_Blog'
+
+urlpatterns = [
+    path('', views.BlogList.as_view(), name='blog_list'),
+    path('write/', views.CreateBlog.as_view() , name='create_blog'),
+    path('details/<slug>', views.blog_details , name='blog_details'),
+    path('liked/<pk>', views.liked , name='liked_post'),
+    path('unlkied/<pk>', views.unliked , name='unliked_post'),
+    path('my-blog/', views.MyBlogs.as_view() , name='my_blogs'),
+    path('edit-blog/<pk>/', views.UpdateBlog.as_view() , name='edit_blog'),
+]
